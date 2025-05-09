c287a304061c0faa3e54711ca6d086024cf7a69e
heshamalmosawi <hishamalmosawii@gmail.com>
2025-05-09 11:53:33 +0300

    refactor: simplify current directory handling in cd function

--- src/modules/features/directory_handling.rs
+++ src/modules/features/directory_handling.rs
@@ -22,75 +22,46 @@ impl Shell {
 
         // setting the path as a string
         let mut abs_dir: String;
-        let mut current_dir: String;
 
         if path.starts_with('/') {
             // meaning user entered absolute path
             abs_dir = path.to_string();
-            current_dir = path.replace(&self.home_dir, "~").to_string();
         } else if path.starts_with('~') {
             // meaning user entered path starting with home dir
             abs_dir = path.replace('~', &self.home_dir).to_string();
-            current_dir = path.replace(&self.home_dir, "~").to_string();
         } else if path.starts_with("../") && !path[3..].starts_with(' ') {
             //  entering directory from parent
             let mut parts: Vec<&str> = self.abs_cwd.split('/').collect();
             parts.pop();
             abs_dir = parts.join("/") + "/" + &path[3..];
-            current_dir = if self.abs_cwd == self.home_dir {
-                // if current path is home, rely on absolute to get the parent
-                let mut parts: Vec<&str> =
-                    self.abs_cwd.split('/').filter(|s| !s.is_empty()).collect();
-                parts.pop();
-                format!("/{:#}", parts.join("/") + "/" + &path[3..])
-            } else {
-                let mut current_parts: Vec<&str> = self.current_dir.split('/').collect();
-                current_parts.pop();
-                current_parts.join("/") + "/" + &path[3..]
-            };
         } else if path.starts_with("..") {
             // entering parent
             let mut parts: Vec<&str> = self.abs_cwd.split('/').collect();
             parts.pop();
             abs_dir = parts.join("/");
-
-            current_dir = if self.abs_cwd == self.home_dir {
-                // if current path is home, rely on absolute to get the parent
-                let mut parts: Vec<&str> =
-                    self.abs_cwd.split('/').filter(|s| !s.is_empty()).collect();
-                parts.pop();
-                format!("/{:#}", parts.join("/"))
-            } else {
-                let mut current_parts: Vec<&str> = self.current_dir.split('/').collect();
-                current_parts.pop();
-                current_parts.join("/")
-            };
         } else {
             abs_dir = if self.abs_cwd == "/" {
                 format!("/{}", path)
             } else {
-                format!("{}/{}", self.abs_cwd, path)
-            };
-
-            current_dir = if self.current_dir == "/" {
-                format!("/{}", path)
-            } else {
-                format!("{}/{}", self.current_dir, path)
+                format!("{}/{}", self.abs_cwd, path.strip_prefix("./").unwrap_or(path))
             };
         }
 
         if abs_dir == "" {
             // if abs dir is empty, the root has entered root
             abs_dir = "/".to_string();
-            current_dir = "/".to_string();
         }
 
         // checking path validity
         path_exists_dir(&abs_dir)?;
 
         // if previous abs path is the home,
+        self.current_dir = if abs_dir.starts_with(&self.home_dir) {
+            abs_dir.replace(&self.home_dir, "~")
+        } else {
+            abs_dir.clone()
+        };
         self.abs_cwd = abs_dir;
-        self.current_dir = current_dir;
 
         Ok(())
     }
